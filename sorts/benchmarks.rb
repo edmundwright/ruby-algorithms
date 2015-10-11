83d70e035c5d242e7134ce955316be6df231a835
Edmund Wright <edmund.wright@gmail.com>
2015-10-11 18:54:03 -0400

    Change benchmarks to use randomly shuffled array.

--- sorts/benchmarks.rb
+++ sorts/benchmarks.rb
@@ -6,7 +6,7 @@ require_relative 'quicksort'
 
 Benchmark.bm do |x|
   [10, 100, 1000, 10000].each do |n|
-    test_array = (0...n).to_a.reverse
+    test_array = (0...n).to_a.shuffle
     x.report("n = #{n}  (merge)".rjust(18)) { merge_sort(test_array.dup) }
     x.report("n = #{n} (bubble)".rjust(18)) { bubble_sort(test_array.dup) }
     x.report("n = #{n}  (quick)".rjust(18)) { quicksort(test_array.dup) }
